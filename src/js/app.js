e69749e799a9f89ea91358f7a0a35885879789f1
cc0803 <simongeldbach@gmail.com>
2024-01-23 19:29:44 +0100

    Load image via Javascript into seperate image element

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 import "../scss/app.scss";
+import image from "../images/goldenGateBridge.jpeg";
 
-console.log("Hello, World!");
+const imageContainer = document.querySelector(".header-background");
+let imageElement = new Image();
+imageElement.src = image;
+imageContainer.appendChild(imageElement);
