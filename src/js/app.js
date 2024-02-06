2bdaffdd12931240fa0260188f74dd6c0ce5c428
cc0803 <simongeldbach@gmail.com>
2024-02-06 19:11:53 +0100

    Add first navbar styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 import "../scss/app.scss";
-import image from "../images/goldenGateBridge.jpeg";
+import "../scss/navigation.scss";
+import background1 from "../images/goldenGateBridge.jpeg";
+import logo from "../images/Logo/LogoSchrift.png";
 
-const imageContainer = document.querySelector(".header-background");
-let imageElement = new Image();
-imageElement.src = image;
-imageContainer.appendChild(imageElement);
+// const imageContainer = document.querySelector(".header-background");
+// let imageElement = new Image();
+// imageElement.src = background1;
+// imageElement.classList.add();
+// imageContainer.appendChild(imageElement);
+
+const logoContainer = document.querySelector(".logo");
+logoContainer.src = logo;
