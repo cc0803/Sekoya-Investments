f8bd2f96611e3d20a90ac445daee547f629b147f
cc0803 <simongeldbach@gmail.com>
2024-02-07 09:51:04 +0100

    Add styling to dropdown menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import "../scss/app.scss";
 import "../scss/navigation.scss";
 import background1 from "../images/goldenGateBridge.jpeg";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
 
 // const imageContainer = document.querySelector(".header-background");
 // let imageElement = new Image();
