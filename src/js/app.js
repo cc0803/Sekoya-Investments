a35117348b6226a8a88bcd910e2918369b71b03b
cc0803 <simongeldbach@gmail.com>
2024-02-10 16:56:39 +0100

    Add styling to second section and alter fontweight

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import "../scss/shared.scss";
 import background1 from "../images/goldenGateBridge.jpeg";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
+import oregonImage from "../images/OregonBeach.jpg";
 
 const image1 = document.querySelector("img.backgroundImage");
 image1.src = background1;
@@ -17,4 +18,10 @@ console.log(dropdownMenu);
 menuButton.addEventListener("click", () => {
 	console.log("Hello Button");
 	dropdownMenu.classList.toggle("visible");
+});
+
+const images = Array.from(document.querySelectorAll(".second-section img"));
+
+images.forEach((img) => {
+	img.src = oregonImage;
 });
