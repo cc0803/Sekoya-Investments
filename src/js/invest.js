0d0e30b81520d73e7c7d3f402281a2f3e5c44ca6
cc0803 <simongeldbach@gmail.com>
2024-02-25 19:03:34 +0100

    Add animation to portfolio companies

--- src/js/invest.js
+++ src/js/invest.js
@@ -4,6 +4,29 @@ import "../scss/footer.scss";
 import "../scss/invest.scss";
 import "../scss/shared.scss";
 import forest from "../images/forest.jpeg";
+import vacation from "../images/History/vacation.jpeg";
 
 const forestImg = document.querySelector(".section-three img");
 forestImg.src = forest;
+
+const vacationImg = Array.from(
+	document.querySelectorAll(".portfolio-company img")
+);
+vacationImg.forEach((img) => {
+	img.src = vacation;
+});
+
+// create Intersection observer
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+		} else {
+			entry.target.classList.remove("show");
+		}
+	});
+});
+
+const portfolioCompanies = document.querySelectorAll(".portfolio-company");
+portfolioCompanies.forEach((company) => observer.observe(company));
