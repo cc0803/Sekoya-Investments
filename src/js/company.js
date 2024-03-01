63f587d405be65863ed269c9d7ae9f19960504c8
cc0803 <simongeldbach@gmail.com>
2024-03-01 19:38:18 +0100

    Add third section to company page

--- src/js/company.js
+++ src/js/company.js
@@ -5,9 +5,13 @@ import "../scss/shared.scss";
 import "../scss/company.scss";
 import nightsky from "../images/nightsky.jpeg";
 import skyline from "../images/skyline.png";
+import man from "../images/History/manOnSign.jpeg";
 
 const nightskyImg = document.querySelector(".one");
 const skylineImg = document.querySelector(".two");
 
 nightskyImg.src = nightsky;
 skylineImg.src = skyline;
+
+const manImg = document.querySelector(".section-three img");
+manImg.src = man;
