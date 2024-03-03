f11f9c86c42ffd6524df96fd03976de19ce686cc
cc0803 <simongeldbach@gmail.com>
2024-03-03 14:37:52 +0100

    Making the footer adaptive

--- src/js/addNavigation.js
+++ src/js/addNavigation.js
@@ -8,7 +8,7 @@ import "../scss/footer.scss";
 const header = document.querySelector("header");
 
 const headerHtml =
-	'<nav class="desktop-nav hidden"><a href="./home.html" class="logo-link"><div class="logoContainer"></div></a><div class="spacer"></div><a href="./company.html" class="page-link">Unternehmen</a><a href="./invest.html" class="page-link">Investieren</a><a href="./geschichte.html" class="page-link">Geschichte</a><div class="spacer"></div><a href="" class="page-link">Log In</a></nav><nav class="mobile"><a href=""><div class="logoContainer"></div></a><div class="spacer"></div><div class="menu-bar-container"><i class="fa-solid fa-bars menu-bar"> </i></div><div class="dropdown-menu"><a href="./company.html" class="page-link">Unternehmen</a><a href="./invest.html" class="page-link">Investieren</a><a href="./geschichte.html" class="page-link">Geschichte</a><a href="" class="page-link">Log In</a></div></nav>';
+	'<nav class="desktop-nav hidden"><a href="./home.html" id="logo-link"><div class="logoContainer"></div></a><div class="spacer"></div><a href="./company.html" class="page-link">Unternehmen</a><a href="./invest.html" class="page-link">Investieren</a><a href="./geschichte.html" class="page-link">Geschichte</a><div class="spacer"></div><a href="" class="page-link">Log In</a></nav><nav class="mobile"><a href=""><div class="logoContainer"></div></a><div class="spacer"></div><div class="menu-bar-container"><i class="fa-solid fa-bars menu-bar"> </i></div><div class="dropdown-menu"><a href="./company.html" class="page-link">Unternehmen</a><a href="./invest.html" class="page-link">Investieren</a><a href="./geschichte.html" class="page-link">Geschichte</a><a href="" class="page-link">Log In</a></div></nav>';
 
 header.innerHTML = headerHtml;
 
@@ -29,6 +29,6 @@ document.head.appendChild(link);
 const footer = document.querySelector("footer");
 
 const footerHtml =
-	'<ul><li>Neuigkeiten</li></ul><ul><li>Kontakt</li></ul><ul><li>Social Media</li><li>LinkedIn</li><li>Instagramm</li><li>X</li></ul><div class="legal flex-center-row"><span class="copyright">@Sekoya Investments Copyright</span><a href="">Kapitalgesellschaft</a><a href="">Steuern</a><a href="./impressum.html">Impressum</a></div>';
+	'<ul><li>Neuigkeiten</li><li><a href="./article.html">Bedeutung des Reisebranche für die Weltwirtschaft</a></li></ul><ul><li>Kontakt</li></ul><ul><li>Social Media</li><li>LinkedIn</li><li>Instagramm</li><li>X</li></ul><div class="legal flex-center-row"><span class="copyright">@Sekoya Investments Copyright</span><a href="./impressum.html">Impressum</a></div>';
 
 footer.innerHTML = footerHtml;
